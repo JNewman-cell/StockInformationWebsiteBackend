dabe7b216a9750b85611ee8ebcd580bf250a3e8e
Jackson Newman <jpnewman167@gmail.com>
2025-11-08 15:07:50 -0800

    Fixed autocomplete query after reorganization and restructuring

--- src/main/java/com/stockInformation/search/repository/SearchRepositoryImpl.java
+++ src/main/java/com/stockInformation/search/repository/SearchRepositoryImpl.java
@@ -23,14 +23,16 @@ public class SearchRepositoryImpl implements SearchRepository {
         String sql = "SELECT t.ticker AS symbol, c.company_name AS name, "
             + "CAST(((CASE WHEN LOWER(t.ticker) = ?1 THEN 1 "
             + "            WHEN LOWER(t.ticker) LIKE CONCAT(?1, '%') THEN 0.95 "
-            + "            ELSE 0.0 END) "
+            + "            ELSE similarity(LOWER(t.ticker), ?1) END) "
             + "       + (CASE WHEN LOWER(c.company_name) = ?1 THEN 1 "
             + "            WHEN LOWER(c.company_name) LIKE CONCAT(?1, '%') THEN 0.95 "
-            + "            ELSE 0.0 END)) / 2.0 AS double precision) AS score "
+            + "            ELSE similarity(LOWER(c.company_name), ?1) END)) / 2.0 AS double precision) AS score "
             + "FROM ticker_summary t "
             + "JOIN cik_lookup c ON t.cik = c.cik "
             + "WHERE LOWER(t.ticker) LIKE CONCAT(?1, '%') "
             + "OR LOWER(c.company_name) LIKE CONCAT(?1, '%') "
+            + "OR similarity(LOWER(t.ticker), ?1) > 0.2 "
+            + "OR similarity(LOWER(c.company_name), ?1) > 0.2 "
             + "ORDER BY score DESC "
             + "LIMIT 10";
 
